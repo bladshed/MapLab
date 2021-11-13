213800264aa2e4b377e913a500ec933921b5bcdc
bladshed <francismarkserrano@yahoo.com>
2021-11-13 10:35:28 +0800

    commit

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
+//------------------------------------------------------------------------------------------------
 // create marker cluster
 // let markerClusterLayer = L.markerClusterGroup();
 
@@ -35,31 +36,91 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // markerClusterLayer.addTo(map);
 
+//------------------------------------------------------------------------------------------------
 // taxi availability and earthquake locator
-window.addEventListener('DOMContentLoaded', async(event)=> {
-    const taxiResponse = await axios.get("https://api.data.gov.sg/v1/transport/taxi-availability");
-    const taxiCoordinates = taxiResponse.data.features[0].geometry.coordinates;
+// window.addEventListener('DOMContentLoaded', async(event)=> {
+//     const taxiResponse = await axios.get("https://api.data.gov.sg/v1/transport/taxi-availability");
+//     const taxiCoordinates = taxiResponse.data.features[0].geometry.coordinates;
 
-    let markerClusterLayer = L.markerClusterGroup();
+//     let markerClusterLayer = L.markerClusterGroup();
 
-    for (let i = 0; i < taxiCoordinates.length; i++) {
-        let pos = taxiCoordinates[i];
-        L.marker([pos[1],pos[0]]).bindPopup(`Lat: ${pos[1]}, Lon: ${pos[1]}`).addTo(markerClusterLayer);
-    }
+//     for (let i = 0; i < taxiCoordinates.length; i++) {
+//         let pos = taxiCoordinates[i];
+//         L.marker([pos[1],pos[0]]).bindPopup(`Lat: ${pos[1]}, Lon: ${pos[1]}`).addTo(markerClusterLayer);
+//     }
 
-    markerClusterLayer.addTo(map);
+//     markerClusterLayer.addTo(map);
 
-    const earthquakeResponse = await axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson ");
-    const earthquakeObjLst = earthquakeResponse.data.features;
-    console.log(earthquakeResponse);
+//     const earthquakeResponse = await axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson ");
+//     const earthquakeObjLst = earthquakeResponse.data.features;
+//     console.log(earthquakeResponse);
 
-    let eqMrkrClusterLayer = L.markerClusterGroup();
+//     let eqMrkrClusterLayer = L.markerClusterGroup();
 
-    for (let i = 0; i < earthquakeObjLst.length; i++) {
-        let info = earthquakeObjLst[i];
-        let earthquakeCoordinates = info.geometry.coordinates;
-        L.marker([earthquakeCoordinates[1],earthquakeCoordinates[0]]).bindPopup(`Earthquake at ${info.properties.place}`).addTo(eqMrkrClusterLayer);
-    }
+//     for (let i = 0; i < earthquakeObjLst.length; i++) {
+//         let info = earthquakeObjLst[i];
+//         let earthquakeCoordinates = info.geometry.coordinates;
+//         L.marker([earthquakeCoordinates[1],earthquakeCoordinates[0]]).bindPopup(`Earthquake at ${info.properties.place}`).addTo(eqMrkrClusterLayer);
+//     }
 
-    eqMrkrClusterLayer.addTo(map);
-});
+//     eqMrkrClusterLayer.addTo(map);
+// });
+
+
+//------------------------------------------------------------------------------------------------
+// ------------- LAB 4 --------------
+//------------------------------------------------------------------------------------------------
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+// layer of markers with pointer icon
+let group= L.layerGroup(); // 1. create the layer group
+L.marker(getRandomLatLng(map)).addTo(group);  // 2. add markers to the group
+L.marker(getRandomLatLng(map)).addTo(group);
+L.marker(getRandomLatLng(map)).addTo(group);
+
+// add the group layer to the map
+group.addTo(map); // 3. add the layer to the map
+
+// layer of markers of circular nature
+let group2 = L.layerGroup();
+for (let i = 0; i < 5; i++) {
+    L.circle(getRandomLatLng(map), {
+        color: 'red',
+        fillColor:"orange",
+        fillOpacity:0.5,
+        radius: 500
+    }).addTo(group2);
+}
+
+let group3 = L.layerGroup();
+for (let i = 0; i < 5; i++) {
+    L.circle(getRandomLatLng(map), {
+        color: 'red',
+        fillColor:"green",
+        fillOpacity:0.5,
+        radius: 250
+    }).addTo(group3);
+}
+
+let baseLayers ={
+    'Markers': group,
+    'Circles': group2
+}
+
+let overlays = {
+    'Green Circle':group3
+}
+
+L.control.layers(baseLayers, overlays).addTo(map);
